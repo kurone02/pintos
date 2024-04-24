614ae23f1b6aad7b30839cee3b24d4d1e4641909
kurone02 <fnt.minhduc@gmail.com>
2024-04-24 16:31:37 +0900

    update: docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 export DOCKER_NAME=pintos
-docker run --rm --name $DOCKER_NAME -v $(pwd):/pintos -it n2sl2019/pintos:v1 bash
+docker run --rm --name $DOCKER_NAME -v $(pwd):/pintos -e TERM=xterm-color -it n2sl2019/pintos:v1 bash
